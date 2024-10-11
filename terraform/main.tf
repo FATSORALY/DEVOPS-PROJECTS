070cd7f36de780838fd9eb648127d7f1d99886b3
Harivelo <arvelandrianantoandro@gmail.com>
2024-10-11 08:03:55 +0300

    deploy infra

---
+++ terraform/main.tf
@@ -0,0 +1,48 @@
+# Spécifier le fournisseur Azure
+provider "azurerm" {
+  features {}
+}
+
+# Créer un groupe de ressources
+resource "azurerm_resource_group" "UBUNTU-SERVER_GROUP" {
+  name     = "UBUNTU-SERVER_GROUP"
+  location = "Korea Central"  # Remplacez par votre région préférée
+}
+
+# Créer un réseau virtuel
+resource "azurerm_virtual_network" "myVNet" {
+  name                = "myVNet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.UBUNTU-SERVER_GROUP.location
+  resource_group_name = azurerm_resource_group.UBUNTU-SERVER_GROUP.name
+}
+
+# Créer un sous-réseau
+resource "azurerm_subnet" "default" {
+  name                 = "default"
+  resource_group_name  = azurerm_resource_group.UBUNTU-SERVER_GROUP.name
+  virtual_network_name = azurerm_virtual_network.myVNet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+# Créer une adresse IP publique
+resource "azurerm_public_ip" "myPublicIP" {  # Changement de nom ici
+  name                = "myPublicIP"
+  location            = azurerm_resource_group.UBUNTU-SERVER_GROUP.location
+  resource_group_name = azurerm_resource_group.UBUNTU-SERVER_GROUP.name
+  allocation_method   = "Static"
+}
+
+# Créer une interface réseau
+resource "azurerm_network_interface" "myNIC" {
+  name                = "myNIC"
+  location            = azurerm_resource_group.UBUNTU-SERVER_GROUP.location
+  resource_group_name = azurerm_resource_group.UBUNTU-SERVER_GROUP.name
+
+  ip_configuration {
+    name                          = "ipconfig1"
+    subnet_id                     = azurerm_subnet.default.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.myPublicIP.id  # Référence mise à jour ici
+  }
+}
---
+++ terraform/variales.tf
@@ -0,0 +1,55 @@
+# Région où sera déployée la ressource
+variable "location" {
+  description = "La région dans laquelle les ressources seront créées."
+  type        = string
+  default     = "Korea Central"  # Remplacez par votre région préférée
+}
+
+# Nom du groupe de ressources
+variable "resource_group_name" {
+  description = "Le nom du groupe de ressources à créer."
+  type        = string
+  default     = "UBUNTU-SERVER_GROUP"
+}
+
+# Nom du réseau virtuel
+variable "virtual_network_name" {
+  description = "Le nom du réseau virtuel."
+  type        = string
+  default     = "myVNet"
+}
+
+# Plage d'adresses du réseau virtuel
+variable "address_space" {
+  description = "L'espace d'adresses pour le réseau virtuel."
+  type        = list(string)
+  default     = ["10.0.0.0/16"]
+}
+
+# Nom du sous-réseau
+variable "subnet_name" {
+  description = "Le nom du sous-réseau à créer."
+  type        = string
+  default     = "default"
+}
+
+# Plage d'adresses du sous-réseau
+variable "subnet_address_prefixes" {
+  description = "Les préfixes d'adresses pour le sous-réseau."
+  type        = list(string)
+  default     = ["10.0.1.0/24"]
+}
+
+# Nom de l'adresse IP publique
+variable "public_ip_name" {
+  description = "Le nom de l'adresse IP publique à créer."
+  type        = string
+  default     = "myPublicIP"
+}
+
+# Nom de l'interface réseau
+variable "network_interface_name" {
+  description = "Le nom de l'interface réseau à créer."
+  type        = string
+  default     = "myNIC"
+}
