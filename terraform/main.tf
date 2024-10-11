3befea54ecdc1221a85286353dbfa34ee69002ec
Harivelo <arvelandrianantoandro@gmail.com>
2024-10-11 10:26:18 +0300

    Add Terraform configuration files

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,10 @@
 # Spécifier le fournisseur Azure
 provider "azurerm" {
   features {}
+  subscription_id = var.subscription_id
+  client_id      = var.client_id
+  client_secret  = var.client_secret
+  tenant_id      = var.tenant_id
 }
 
 # Créer un groupe de ressources
